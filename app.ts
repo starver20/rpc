0fd9ec360182ddc92a95efe1a7ed09a8ce08018b
amarnarute <amarnarute2002@gmail.com>
2021-03-26 13:42:07 +0530

    loop over multiple times for p0

--- app.ts
+++ app.ts
@@ -6,17 +6,16 @@ dotenv.config();
 
 let arr: {
   pValues: number[];
-  p0: number[];
-  p1: number[];
-  p2: number[];
-  p3: number[];
+  p: number[][];
 }[] = [
   {
     pValues: [0, 0, 0, 0],
-    p0: [0, 0, 0, 0],
-    p1: [0, 0, 0, 0],
-    p2: [0, 0, 0, 0],
-    p3: [0, 0, 0, 0],
+    p: [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
   },
 ];
 
@@ -24,47 +23,49 @@ app.get('/', async (req: Request, res: Response) => {
   res.send('Go to /game/start');
 });
 app.get('/game/start', async (req: Request, res: Response) => {
-  arr[0].pValues[0] = Math.floor(Math.random() * 3 + 1);
-  arr[0].pValues[1] = Math.floor(Math.random() * 3 + 1);
-  arr[0].pValues[2] = Math.floor(Math.random() * 3 + 1);
-  arr[0].pValues[3] = Math.floor(Math.random() * 3 + 1);
+  for (let j = 0; j < 2; j++) {
+    arr[0].pValues[0] = Math.floor(Math.random() * 3 + 1);
+    arr[0].pValues[1] = Math.floor(Math.random() * 3 + 1);
+    arr[0].pValues[2] = Math.floor(Math.random() * 3 + 1);
+    arr[0].pValues[3] = Math.floor(Math.random() * 3 + 1);
 
-  // arr[0].pValues[0] = 1;
-  // arr[0].pValues[1] = 6;
-  // arr[0].pValues[2] = 2;
-  // arr[0].pValues[3] = 3;
-  if (arr[0].pValues[0] === 1) {
-    if (arr[0].pValues[1] === 3) {
-      arr[0].p0[1]++;
+    // arr[0].pValues[0] = 1;
+    // arr[0].pValues[1] = 6;
+    // arr[0].pValues[2] = 2;
+    // arr[0].pValues[3] = 3;
+    if (arr[0].pValues[0] === 1) {
+      if (arr[0].pValues[1] === 3) {
+        arr[0].p[0][1]++;
+      }
+      if (arr[0].pValues[2] === 3) {
+        arr[0].p[0][2]++;
+      }
+      if (arr[0].pValues[3] === 3) {
+        arr[0].p[0][3]++;
+      }
+    } else if (arr[0].pValues[0] === 2) {
+      if (arr[0].pValues[1] === 1) {
+        arr[0].p[0][1]++;
+      }
+      if (arr[0].pValues[2] === 1) {
+        arr[0].p[0][2]++;
+      }
+      if (arr[0].pValues[3] === 1) {
+        arr[0].p[0][3]++;
+      }
+    } else if (arr[0].pValues[0] === 3) {
+      if (arr[0].pValues[1] === 2) {
+        arr[0].p[0][1]++;
+      }
+      if (arr[0].pValues[2] === 2) {
+        arr[0].p[0][2]++;
+      }
+      if (arr[0].pValues[3] === 2) {
+        arr[0].p[0][3]++;
+      }
     }
-    if (arr[0].pValues[2] === 3) {
-      arr[0].p0[2]++;
-    }
-    if (arr[0].pValues[3] === 3) {
-      arr[0].p0[3]++;
-    }
-  } else if (arr[0].pValues[0] === 2) {
-    if (arr[0].pValues[1] === 1) {
-      arr[0].p0[1]++;
-    }
-    if (arr[0].pValues[2] === 1) {
-      arr[0].p0[2]++;
-    }
-    if (arr[0].pValues[3] === 1) {
-      arr[0].p0[3]++;
-    }
-  } else if (arr[0].pValues[0] === 3) {
-    if (arr[0].pValues[1] === 2) {
-      arr[0].p1[0]++;
-    }
-    if (arr[0].pValues[2] === 2) {
-      arr[0].p0[2]++;
-    }
-    if (arr[0].pValues[3] === 2) {
-      arr[0].p0[3]++;
-    }
+    console.log(arr);
   }
-
   res.send(arr);
 });
 
