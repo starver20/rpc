cbe3a190617ce99532f9270ca0d04589ef93564b
amarnarute <amarnarute2002@gmail.com>
2021-03-26 13:19:35 +0530

    basic concept for p0

--- app.ts
+++ app.ts
@@ -33,40 +33,35 @@ app.get('/game/start', async (req: Request, res: Response) => {
   // arr[0].pValues[1] = 6;
   // arr[0].pValues[2] = 2;
   // arr[0].pValues[3] = 3;
-
   if (arr[0].pValues[0] === 1) {
+    if (arr[0].pValues[1] === 3) {
+      arr[0].p0[1]++;
+    }
+    if (arr[0].pValues[2] === 3) {
+      arr[0].p0[2]++;
+    }
+    if (arr[0].pValues[3] === 3) {
+      arr[0].p0[3]++;
+    }
+  } else if (arr[0].pValues[0] === 2) {
+    if (arr[0].pValues[1] === 1) {
+      arr[0].p0[1]++;
+    }
+    if (arr[0].pValues[2] === 1) {
+      arr[0].p0[2]++;
+    }
+    if (arr[0].pValues[3] === 1) {
+      arr[0].p0[3]++;
+    }
+  } else if (arr[0].pValues[0] === 3) {
     if (arr[0].pValues[1] === 2) {
       arr[0].p1[0]++;
     }
     if (arr[0].pValues[2] === 2) {
-      arr[0].p2[0]++;
+      arr[0].p0[2]++;
     }
     if (arr[0].pValues[3] === 2) {
-      arr[0].p3[0]++;
-    }
-  }
-
-  if (arr[0].pValues[0] === 2) {
-    if (arr[0].pValues[1] === 3) {
-      arr[0].p1[0]++;
-    }
-    if (arr[0].pValues[2] === 3) {
-      arr[0].p2[0]++;
-    }
-    if (arr[0].pValues[3] === 3) {
-      arr[0].p3[0]++;
-    }
-  }
-
-  if (arr[0].pValues[0] === 3) {
-    if (arr[0].pValues[1] === 1) {
-      arr[0].p1[0]++;
-    }
-    if (arr[0].pValues[2] === 1) {
-      arr[0].p2[0]++;
-    }
-    if (arr[0].pValues[3] === 1) {
-      arr[0].p3[0]++;
+      arr[0].p0[3]++;
     }
   }
 
