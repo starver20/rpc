6621c64c8010dddddd66121d5aa8f0e177c0e6f3
amarnarute <amarnarute2002@gmail.com>
2021-03-26 16:21:19 +0530

    complete

--- app.ts
+++ app.ts
@@ -19,11 +19,13 @@ let arr: {
   },
 ];
 
+let arrItr: any = [];
+
 app.get('/', async (req: Request, res: Response) => {
   res.send('Go to /game/start');
 });
 app.get('/game/start', async (req: Request, res: Response) => {
-  for (let j = 0; j < 2; j++) {
+  for (let j = 0; j < 50; j++) {
     arr[0].pValues[0] = Math.floor(Math.random() * 3 + 1);
     arr[0].pValues[1] = Math.floor(Math.random() * 3 + 1);
     arr[0].pValues[2] = Math.floor(Math.random() * 3 + 1);
@@ -33,40 +35,53 @@ app.get('/game/start', async (req: Request, res: Response) => {
     // arr[0].pValues[1] = 6;
     // arr[0].pValues[2] = 2;
     // arr[0].pValues[3] = 3;
-    if (arr[0].pValues[0] === 1) {
-      if (arr[0].pValues[1] === 3) {
-        arr[0].p[0][1]++;
-      }
-      if (arr[0].pValues[2] === 3) {
-        arr[0].p[0][2]++;
-      }
-      if (arr[0].pValues[3] === 3) {
-        arr[0].p[0][3]++;
-      }
-    } else if (arr[0].pValues[0] === 2) {
-      if (arr[0].pValues[1] === 1) {
-        arr[0].p[0][1]++;
-      }
-      if (arr[0].pValues[2] === 1) {
-        arr[0].p[0][2]++;
-      }
-      if (arr[0].pValues[3] === 1) {
-        arr[0].p[0][3]++;
-      }
-    } else if (arr[0].pValues[0] === 3) {
-      if (arr[0].pValues[1] === 2) {
-        arr[0].p[0][1]++;
-      }
-      if (arr[0].pValues[2] === 2) {
-        arr[0].p[0][2]++;
-      }
-      if (arr[0].pValues[3] === 2) {
-        arr[0].p[0][3]++;
+    for (let i = 0; i < 4; i++) {
+      if (arr[0].pValues[i] === 1) {
+        if (arr[0].pValues[0] === 3) {
+          arr[0].p[i][0]++;
+        }
+        if (arr[0].pValues[1] === 3) {
+          arr[0].p[i][1]++;
+        }
+        if (arr[0].pValues[2] === 3) {
+          arr[0].p[i][2]++;
+        }
+        if (arr[0].pValues[3] === 3) {
+          arr[0].p[i][3]++;
+        }
+      } else if (arr[0].pValues[i] === 2) {
+        if (arr[0].pValues[0] === 1) {
+          arr[0].p[i][0]++;
+        }
+        if (arr[0].pValues[1] === 1) {
+          arr[0].p[i][1]++;
+        }
+        if (arr[0].pValues[2] === 1) {
+          arr[0].p[i][2]++;
+        }
+        if (arr[0].pValues[3] === 1) {
+          arr[0].p[i][3]++;
+        }
+      } else if (arr[0].pValues[i] === 3) {
+        if (arr[0].pValues[0] === 2) {
+          arr[0].p[i][0]++;
+        }
+        if (arr[0].pValues[1] === 2) {
+          arr[0].p[i][1]++;
+        }
+        if (arr[0].pValues[2] === 2) {
+          arr[0].p[i][2]++;
+        }
+        if (arr[0].pValues[3] === 2) {
+          arr[0].p[i][3]++;
+        }
       }
     }
+    arrItr[j] = JSON.parse(JSON.stringify(arr));
     console.log(arr);
   }
-  res.send(arr);
+  console.log(arrItr);
+  res.send(arrItr);
 });
 
 app.listen(process.env.PORT, () => {
